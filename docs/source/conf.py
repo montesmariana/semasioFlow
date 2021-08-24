687c4ad7e73ad8fba771a1d49543bd137a6a92bc
montesmariana <mariana.montes@kuleuven.be>
2021-08-24 17:40:24 +0200

    fix docs to read numpy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,8 @@ release = '0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
